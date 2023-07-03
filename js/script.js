fdffe89b6f2bba99440d26f68d70112bab7cd6c5
OverInk <dostoevskiy201@mail.ru>
2023-07-03 12:00:23 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict"
 
-
 document.querySelectorAll('.team').forEach(e => {
 	e.addEventListener("click", e => {
 		const listPlayer = e.currentTarget.dataset.path;
@@ -8,6 +7,25 @@ document.querySelectorAll('.team').forEach(e => {
 		document.querySelectorAll('.js__button').forEach(e => {
 			if (!document.querySelector(`[data-target=${listPlayer}]`).classList.contains('open')) {
 				document.querySelector(`[data-target=${listPlayer}]`).classList.add('active');
+			}
+			// window.onclick = e => {
+			// 	if (e.target == document.querySelector(`[data-target=${listPlayer}]`)) {
+			// 		return;
+			// 	} else {
+			// 		document.querySelector(`[data-target=${listPlayer}]`).classList.remove('active');
+			// 	}
+			// }
+		});
+	});
+});
+
+document.querySelectorAll('.btn-cancel').forEach(e => {
+	e.addEventListener("click", e => {
+		const close = e.currentTarget.dataset.close;
+		console.log(close);
+		document.querySelectorAll('.js__button').forEach(e => {
+			if (!document.querySelector(`[data-target=${close}]`).classList.contains('open')) {
+				document.querySelector(`[data-target=${close}]`).classList.remove('active');
 			}
 		});
 	});
